8274bdacb974bd798606f10c76bcbff80fdd1f4a
dhianiwidiya <dhianiwp@gmail.com>
2022-11-17 15:23:11 +0700

    masih error di input phone number

--- src/main/java/EmulatorAndroid.java
+++ src/main/java/EmulatorAndroid.java
@@ -1,25 +1,36 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class EmulatorAndroid {
-    public static void main (String[] args) throws Exception {
+    public static void main (String[] args) throws MalformedURLException, InterruptedException {
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
         dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");//Android Emulator
-        dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        dc.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
         dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0"); //Version of Android ex: 9.0 (pie)
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         dc.setCapability("appPackage", "id.danafix.ios");
         dc.setCapability("appActivity", "id.danafix.mobile.danafix.MainActivity");
-
+        // dc.setCapability(MobileCapabilityType.APP, "D:\\app-danafix\\mobile-dev-221115-1.apk");
         URL url = new URL("http://localhost:4723/wd/hub");
         AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(url,dc);
-        String sessionId = driver.getSessionId().toString();
-        //Thread.sleep(5000);
+        Thread.sleep(5000);
+        System.out.println("Install Success");
+
+        WebElement skip = driver.findElement(By.xpath("//android.view.View[@content-desc=\"Skip\"]"));
+        skip.click();
+        WebElement inputphonenumber = driver.findElement(By.className("android.widget.EditText"));
+        inputphonenumber.click();
+        String text = inputphonenumber.getText();
+       // WebElement login = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Login\"]"));
+       // login.click();
 
     }
 }
--- src/main/java/InstallApkAndroid.java
+++ src/main/java/InstallApkAndroid.java
@@ -1,6 +1,5 @@
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebElement;
@@ -14,7 +13,7 @@ public class InstallApkAndroid {
         dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0"); //Version of Android ex: 9.0 (pie)
         dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
         //dc.setCapability(MobileCapabilityType.APP, "D:\\app-danafix\\mobile-2022-11-12-1020.apk");
-        dc.setCapability(MobileCapabilityType.APP, "D:\\app-danafix\\mobile-dev-221115-1.apk");
+        dc.setCapability(MobileCapabilityType.APP, "D:\\app-danafix\\mobile-dev-221117-1 (1).apk");
         URL url=new URL("http://localhost:4723/wd/hub");
 
         AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(url,dc);

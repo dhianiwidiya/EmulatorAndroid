c24ca4ae55f6f6d0646379cda26b01e54ac91d98
dhianiwidiya <dhianiwp@gmail.com>
2022-11-19 15:38:44 +0700

    tambahan file baru

--- src/main/java/EmulatorAndroid.java
+++ src/main/java/EmulatorAndroid.java
@@ -16,19 +16,24 @@ public class EmulatorAndroid {
         dc.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
         dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0"); //Version of Android ex: 9.0 (pie)
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+       // dc.setCapability(MobileCapabilityType.APP, "D:\\app-danafix\\mobile-dev-221117-2.apk");
         dc.setCapability("appPackage", "id.danafix.ios");
         dc.setCapability("appActivity", "id.danafix.mobile.danafix.MainActivity");
-        // dc.setCapability(MobileCapabilityType.APP, "D:\\app-danafix\\mobile-dev-221115-1.apk");
+        dc.setCapability("ensureWebviewsHavePages", true);
+        dc.setCapability("nativeWebScreenshot", true);
+        dc.setCapability("newCommandTimeout", 3600);
+        dc.setCapability("connectHardwareKeyboard", true);
+
         URL url = new URL("http://localhost:4723/wd/hub");
         AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(url,dc);
-        Thread.sleep(5000);
         System.out.println("Install Success");
 
         WebElement skip = driver.findElement(By.xpath("//android.view.View[@content-desc=\"Skip\"]"));
         skip.click();
-        WebElement inputphonenumber = driver.findElement(By.className("android.widget.EditText"));
-        inputphonenumber.click();
-        String text = inputphonenumber.getText();
+        //WebElement inputphonenumber = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText"));
+        //inputphonenumber.sendKeys("0809791539187");
+        //inputphonenumber.click();
+        //String text = inputphonenumber.getText();
        // WebElement login = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Login\"]"));
        // login.click();
 
